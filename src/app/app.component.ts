59272ddb4c5f4272520d43e68bb043fb700d5734
Julio Ulloa <julioulloa16@gmail.com>
2020-09-19 22:46:14 -0600

    ejercicio1Guia4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Alumno } from './models/alumno';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crudAngular';
+
+  alumnoArray:Alumno[] = [
+    {id:1,name:"alex",lastName:"campos",age:35},
+    {id:2,name:"maria",lastName:"lopez",age:20},
+    {id:3,name:"juan",lastName:"castro",age:25}    
+
+  ]
+
+  selectedAlumno:Alumno = {id:0,name:"",lastName:"",age:0};
+
+  openForEdit(alumno:Alumno):void{
+
+    this.selectedAlumno = alumno;
+  }
+
+  addOrEdit():void{
+
+    if(this.selectedAlumno.id ===0){
+
+      this.selectedAlumno.id= this.alumnoArray.length +1;
+      this.alumnoArray.push(this.selectedAlumno);
+    }
+
+    this.selectedAlumno = {id:0,name:"",lastName:"",age:0};
+  }
+
+  delete():void{
+
+    this.alumnoArray = this.alumnoArray.filter(x=>x != this.selectedAlumno);
+    this.selectedAlumno = {id:0,name:"",lastName:"",age:0};
+  }
 }
+
---
+++ src/app/models/alumno.ts
@@ -0,0 +1,6 @@
+export class Alumno {
+    id:number = 0;
+    name:string = '';
+    lastName:string = '';
+    age:number = 0;
+}
